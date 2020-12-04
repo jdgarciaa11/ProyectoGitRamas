ae9d8e40e3b7b7379b43f5ac9357d4292d6337b1
Jesus Garcia <jesusd.garcia@iesnervion.es>
2020-12-04 12:17:00 +0100

    Saludo por Hola

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Saludos Mundo!");
         imprimeSaludo();
     }
 }

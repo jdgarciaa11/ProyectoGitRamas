92cda1b778f388ade28fb8d58a0eee5a0fcecacb
Jesus Garcia <jesusd.garcia@iesnervion.es>
2020-12-04 12:07:22 +0100

    Correccion de imprime saludo y implementacion de imprime saludo.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
-public static void imprimeSaludo() { 
-    System.out.println(“Hei”);	
-}
 
 public class Main {
+    public static void imprimeSaludo() {
+        System.out.println("Hei");
+    }
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        imprimeSaludo();
     }
 }
